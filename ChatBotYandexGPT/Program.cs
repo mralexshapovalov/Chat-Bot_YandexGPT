b6f89f24d4b3bfa52b3838691a4422dfac5881c4
Alexandr <a.a.shapovov@bk.ru>
2024-03-11 13:48:59 +0300

    Test

--- ChatBotYandexGPT/Program.cs
+++ ChatBotYandexGPT/Program.cs
@@ -15,13 +15,15 @@ namespace ChatBotYandexGPT
     {
         static async Task Main(string[] args)
         {
-            var modelUri = "";
+            Console.WriteLine("Это чат-бот работает на базе Yandex GPT");
+            var modelUri = "gpt://b1gnhp30ce0i8c5b3l7d/yandexgpt-lite";
             var url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion";
-            var apiKey = "";
+            var apiKey = "AQVNyJEC3OglIiOsUXafvwK0oQdOfKkdhYOLPEhH";
             string l = "";
            
             while (true)
             {
+                Console.Write("Вопрос : ");
                 var prompt = new
                 {
                     modelUri,
@@ -50,11 +52,10 @@ namespace ChatBotYandexGPT
                         httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Api-Key", apiKey);
                         var response = await httpClient.PostAsync(url, content);
                         var result = await response.Content.ReadAsStringAsync();
-                        
-                      
-                        
-                        string k = result.ToString();
-                        Console.WriteLine(k);
+
+                        string k = result.Replace("{\"result\":{\"alternatives\":[{\"message\":{\"role\":\"assistant\",\"text\":\"",String.Empty);
+                        k = k.Substring(0, k.IndexOf("\"}"));
+                        Console.Write("Ответ: " + k + "\n");
                     }
                 }
                 else
